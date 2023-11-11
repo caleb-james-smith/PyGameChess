af54bbdfadad86a629207dda7b3f91d882fa6c17
Caleb James Smith <colorado.caleb.smith@gmail.com>
2023-11-11 12:20:18 -0600

    First attempt at drawing pieces; this needs some work.

--- play.py
+++ play.py
@@ -4,17 +4,23 @@
 import pygame
 
 # Draw a solid square
-# x: x position measured from left edge
-# y: y position measured from top edge
-def draw_square(my_game, my_screen, color, x, y, side):
-    my_game.draw.rect(my_screen, color, [x, y, side, side], 0)
+# x_position: x position measured from left edge
+# y_position: y position measured from top edge
+def draw_square(my_game, my_screen, color, x_position, y_position, side):
+    my_game.draw.rect(my_screen, color, [x_position, y_position, side, side], 0)
+
+# Draw a piece
+# x_position: x position measured from left edge
+# y_position: y position measured from top edge
+def draw_piece(my_game, my_screen, color, x_position, y_position, size):
+    my_game.draw.circle(my_screen, color, [x_position, y_position], size, 0)
 
 # Get square color based on x, y coordinate indices
 def get_square_color(x, y, light_color, dark_color):
     # Use parity to determine square color
     parity = (x + y) % 2
     # Odd parity: dark color
-    if(parity):
+    if parity:
         return dark_color
     # Even parity: light color
     else:
@@ -33,6 +39,22 @@ def draw_board(my_game, my_screen, light_color, dark_color, squares_per_side, sq
             # Draw square
             draw_square(my_game, my_screen, color, x_position, y_position, square_side)
 
+# Draw the pieces
+def draw_pieces(my_game, my_screen, light_color, dark_color, squares_per_side, square_side):
+    # Draw pieces
+    for x in range(squares_per_side):
+        for y in range(squares_per_side):
+            # FIXME: draw pieces using both colors
+            color = light_color
+            # Get piece position and size; probably different than square position (FIXME)
+            x_position = x * square_side
+            y_position = y * square_side
+            # FIXME: size should be smaller than square side
+            size = square_side / 2
+            # Draw piece
+            draw_piece(my_game, my_screen, color, x_position, y_position, size)
+
+# Run the game
 def run_game():
     # Initialize pygame
     pygame.init()
@@ -40,6 +62,8 @@ def run_game():
     # Define colors
     PURE_WHITE  = (255, 255, 255)
     PURE_BLACK  = (0, 0, 0)
+    #LIGHT_COLOR = PURE_WHITE
+    #DARK_COLOR  = PURE_BLACK
     LIGHT_COLOR = (233, 237, 204)
     DARK_COLOR  = (119, 153, 84)
 
@@ -66,6 +90,10 @@ def run_game():
         # Draw the board
         draw_board(pygame, screen, LIGHT_COLOR, DARK_COLOR, SQUARES_PER_SIDE, SQUARE_SIDE)
         
+        # Draw the pieces
+        # FIXME: piece colors should be different than board colors
+        draw_pieces(pygame, screen, LIGHT_COLOR, DARK_COLOR, SQUARES_PER_SIDE, SQUARE_SIDE)
+
         # Flip the display
         pygame.display.flip()
 
