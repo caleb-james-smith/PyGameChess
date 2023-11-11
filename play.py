5e8399b2f3534c357f8312e6dd2205ecb9033529
Caleb James Smith <colorado.caleb.smith@gmail.com>
2023-11-10 22:17:22 -0600

    Adjust colors. Remove old code.

--- play.py
+++ play.py
@@ -4,6 +4,8 @@
 import pygame
 
 # Draw a solid square
+# x: x position measured from left edge
+# y: y position measured from top edge
 def draw_square(my_game, my_screen, color, x, y, side):
     my_game.draw.rect(my_screen, color, [x, y, side, side], 0)
 
@@ -12,14 +14,17 @@ def run_game():
     pygame.init()
 
     # Define colors
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
+    #LIGHT_COLOR = (255, 255, 255)
+    #DARK_COLOR = (0, 0, 0)
+    LIGHT_COLOR = (233, 237, 204)
+    DARK_COLOR  = (119, 153, 84)
 
     # Set up the drawing window (screen)
-    SCREEN_WIDTH  = 800
-    SCREEN_HEIGHT = 800
-    BOARD_SIDE    = 800
-    SQUARE_SIDE   = 100
+    SCREEN_WIDTH        = 800
+    SCREEN_HEIGHT       = 800
+    SQUARE_SIDE         = 100
+    SQUARES_PER_SIDE    = 8
+    BOARD_SIDE          = SQUARES_PER_SIDE * SQUARE_SIDE
     screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
     # Run until the user asks to quit
@@ -32,24 +37,10 @@ def run_game():
                 running = False
 
         # Fill the background with white
-        screen.fill(WHITE)
-
-        # Draw a solid blue circle in the center
-        #pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-
-        # Draw a rectangle using rect()
-        # parameters: surface, color, [x, y, width, height]
-        # x: x position measured from left edge
-        # y: y position measured from top edge
-        #pygame.draw.rect(screen, BLACK, [0, 0, 100, 200], 0)
-
-        #for x in range(0, 800, 200):
-        #    draw_square(pygame, screen, BLACK, x, 0, 100)
-        #for x in range(100, 800, 200):
-        #    draw_square(pygame, screen, BLACK, x, 100, 100)
+        screen.fill(LIGHT_COLOR)
 
         # draw black squares
-        for row in range(8):
+        for row in range(SQUARES_PER_SIDE):
             y = row * SQUARE_SIDE
             # determine starting x value for each row
             if row % 2 == 0:
@@ -57,7 +48,7 @@ def run_game():
             else:
                 x_start = 0
             for x in range(x_start, BOARD_SIDE, 2 * SQUARE_SIDE):
-                draw_square(pygame, screen, BLACK, x, y, SQUARE_SIDE)
+                draw_square(pygame, screen, DARK_COLOR, x, y, SQUARE_SIDE)
 
         # Flip the display
         pygame.display.flip()
