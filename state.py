01a1ccaed891c38e30403f5864573af98206c310
Caleb James Smith <colorado.caleb.smith@gmail.com>
2023-11-13 21:08:29 -0600

    State class to save game state; only an example now.

---
+++ state.py
@@ -0,0 +1,35 @@
+# State class
+
+# Save the current game state
+# - Use 8x8 matrix to represent squares on chess board
+# - White pieces are positive
+# - Black pieces are negative
+# - 0: empty squares are 0
+# - 1: pawn
+# - 2: knight
+# - 3: bishop
+# - 4: rook
+# - 5: queen
+# - 6: king
+
+class State:
+    def __init__(self, state):
+        self.state = state
+
+    def GetState(self):
+        return self.state
+
+    def SetState(self, state):
+        self.state = state
+
+    def PrintState(self):
+        print("State: {0}".format(self.state))
+
+def main():
+    state = State("Tacos.")
+    state.PrintState()
+    state.SetState("Burritos.")
+    print("State: {0}".format(state.GetState()))
+
+if __name__ == "__main__":
+    main()
