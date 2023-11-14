1483427930ed5dda070e65679fd41b974b4343b3
Caleb James Smith <colorado.caleb.smith@gmail.com>
2023-11-14 11:23:39 -0600

    Improve format for printing the game state.

--- state.py
+++ state.py
@@ -12,6 +12,7 @@
 # - 5: queen
 # - 6: king
 
+# class to define current game state (piece positions)
 class State:
     def __init__(self, state=None):
         self.state = state
@@ -35,27 +36,41 @@ class State:
     def SetState(self, state):
         self.state = state
 
+    # print the state with pretty formatting
     def PrintState(self):
+        # number of dashes to match length of row string
+        n_dashes = (8 * 3) - 1
+        line = n_dashes * "-"
+
         print("State:")
         if self.state:
+            print(line)
             for row in self.state:
-                print(row)
+                # fill with whitespace using string rjust()
+                # forces positive and negative ints < 10 to use the same width
+                row_formatted = [str(value).rjust(2) for value in row]
+                row_string = ",".join(row_formatted)
+                print(row_string)
+            print(line)
         else:
             print(self.state)
 
+    # set initial state (starting position)
     def SetInitialState(self):
+        # initial state: chess starting position
         state = [
             [-4, -2, -3, -5, -6, -3, -2, -4],
             [-1, -1, -1, -1, -1, -1, -1, -1],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [1, 1, 1, 1, 1, 1, 1, 1],
-            [4, 2, 3, 5, 6, 3, 2, 4]
+            [ 0,  0,  0,  0,  0,  0,  0,  0],
+            [ 0,  0,  0,  0,  0,  0,  0,  0],
+            [ 0,  0,  0,  0,  0,  0,  0,  0],
+            [ 0,  0,  0,  0,  0,  0,  0,  0],
+            [ 1,  1,  1,  1,  1,  1,  1,  1],
+            [ 4,  2,  3,  5,  6,  3,  2,  4]
         ]
         self.SetState(state)
 
+    # get name of piece based on value
     def GetPieceName(self, value):
         result = ""
         
