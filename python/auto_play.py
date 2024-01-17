c5bd0455b8b57a5701160055c7d50a4c3634711b
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-01-16 21:03:25 -0600

    Update auto play: test different agents.

--- python/auto_play.py
+++ python/auto_play.py
@@ -7,7 +7,9 @@ import time
 from board import Board
 from state import State
 from player import Player
-from agent import AgentRandom, AgentCapture
+from tables import PieceTable
+from evaluate import EvaluateMaterial, EvaluatePosition
+from agent import AgentRandom, AgentCapture, AgentMinimax
 
 # Run the game
 def run_game():
@@ -44,11 +46,30 @@ def run_game():
     screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
     # Initialize the board
     board = Board(pygame, screen, BOARD_LIGHT_COLOR, BOARD_DARK_COLOR, SQUARES_PER_SIDE, SQUARE_SIDE)
+    # Create evaluators
+    piece_table = PieceTable()
+    
+    #game_evaluator = EvaluateMaterial()
+    game_evaluator = EvaluatePosition(piece_table)
+    
+    #white_evaluator = EvaluateMaterial()
+    white_evaluator = EvaluatePosition(piece_table)
+    
+    #black_evaluator = EvaluateMaterial()
+    black_evaluator = EvaluatePosition(piece_table)
+    
     # Create agents
+    time_delay  = 0.0
+    max_depth   = 1
+    
+    black_agent = AgentRandom()
+    #black_agent = AgentCapture()
+    #black_agent = AgentMinimax(black_evaluator, max_depth)
+    
     #white_agent = AgentRandom()
-    #black_agent = AgentRandom()
-    white_agent = AgentCapture()
-    black_agent = AgentCapture()
+    #white_agent = AgentCapture()
+    white_agent = AgentMinimax(white_evaluator, max_depth)
+    
     # Create players
     white_player = Player("Merry",  "white", white_agent)
     black_player = Player("Pippin", "black", black_agent)
@@ -62,7 +83,7 @@ def run_game():
     current_player  = state.GetCurrentPlayer()
     opposing_player = state.GetOpposingPlayer()
     # Print detailed game state
-    state.PrintGameState()
+    state.PrintGameState(game_evaluator)
     
     # Fill the background with white
     screen.fill(PURE_WHITE)
@@ -76,7 +97,8 @@ def run_game():
     # Game running condition
     running = True
     # Add a time delay... take a breathe. :)
-    time.sleep(0.5)
+    if time_delay:
+        time.sleep(time_delay)
     
     # Run until the user asks to quit
     while running:
@@ -86,16 +108,22 @@ def run_game():
             if event.type == pygame.QUIT:
                 running = False
         
-        # Choose a legal move
+        # Let the agent choose a move
         current_agent = current_player.GetAgent()
-        chosen_move   = current_agent.ChooseMove(state, current_player, opposing_player)
+        start_time  = time.time()
+        chosen_move = current_agent.ChooseMove(state, current_player, opposing_player)
+        end_time    = time.time()
+        calc_time   = end_time - start_time
         
         # Check that the move is not empty
         if chosen_move:
             print("Chosen move: {0}".format(chosen_move))
+            print("Calculation time: {0:.3f} seconds".format(calc_time))
             # Get move positions
             position_from, position_to = state.board.GetMovePositions(chosen_move)
-            print("position_from: {0}, position_to: {1}".format(position_from, position_to))
+            agent_move_position = position_to
+            #print("position_from: {0}, position_to: {1}".format(position_from, position_to))
+            
             # Get piece to move!
             piece_to_move = state.GetPieceInPosition(position_from)
             # Move piece
@@ -107,16 +135,27 @@ def run_game():
             current_player  = state.GetCurrentPlayer()
             opposing_player = state.GetOpposingPlayer()
             # Print detailed game state
-            state.PrintGameState()
+            state.PrintGameState(game_evaluator)
             # Add a time delay... take a breathe. :)
-            time.sleep(0.5)
+            if time_delay:
+                time.sleep(time_delay)
         
         # Fill the background with white
         screen.fill(PURE_WHITE)
+        
         # Draw the board
         board.DrawBoard()
+
+        # Draw the latest agent move
+        if agent_move_position:
+            agent_square_position = board.GetSquarePosition(agent_move_position)
+            square_x = agent_square_position[0]
+            square_y = agent_square_position[1]
+            board.DrawSquare(CLICK_COLOR_PIECE, square_x, square_y)
+        
         # Draw the pieces
         state.DrawPieces(pygame, screen, PIECE_LIGHT_COLOR, PIECE_DARK_COLOR, PIECE_BORDER_COLOR, SQUARES_PER_SIDE, SQUARE_SIDE)
+        
         # Flip the display
         pygame.display.flip()
     
